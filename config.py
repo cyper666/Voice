6a193abd9b3a30c939feab1b7b95dc3f83da6882
cyper666 <79481652+cyper666@users.noreply.github.com>
2021-04-13 10:07:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 jio_saavn_api = "http://127.0.0.1:5000/result/?query="
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = "-1001176749967"
-owner_id = 1327934109
+sudo_chat_id = "-1001266612390"
+owner_id = 1553960336
